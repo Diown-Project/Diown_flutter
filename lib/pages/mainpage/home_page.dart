9af8d630e2bc2a39fda914235126cd312a56c8f4
woradon <oat_45122@hotmail.com>
2022-04-07 01:06:22 +0700

    home

--- lib/pages/mainpage/home_page.dart
+++ lib/pages/mainpage/home_page.dart
@@ -3,6 +3,7 @@ import 'package:diown/pages/putdowndiary/diarydetailputdown.dart';
 import 'package:http/http.dart' as http;
 import 'package:diown/pages/event/carousel_loading.dart';
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -48,8 +49,8 @@ class _HomePageState extends State<HomePage> {
                         CarouselLoading(),
                         Container(
                             alignment: Alignment.topLeft,
-                            child: Padding(
-                              padding: const EdgeInsets.fromLTRB(14, 8, 14, 0),
+                            child: const Padding(
+                              padding: EdgeInsets.fromLTRB(14, 8, 14, 0),
                               child: Text(
                                 'Latest following putdown:',
                                 style: TextStyle(
@@ -57,56 +58,66 @@ class _HomePageState extends State<HomePage> {
                                 ),
                               ),
                             )),
+                        const SizedBox(height: 15),
                         Column(
                           children: latest.map<Widget>((e) {
-                            return ListTile(
-                              leading: CircleAvatar(
-                                backgroundImage: NetworkImage(
-                                    'https://storage.googleapis.com/noseason/${e['user_detail'][0]['profile_image']}'),
-                              ),
-                              title: e['topic'] != null
-                                  ? Text(e['topic'])
-                                  : Text(
-                                      '${e['mood_emoji']} ${e['mood_detail']}'),
-                              subtitle: Row(
-                                children: [
-                                  Flexible(
-                                    child: Text(
-                                      '@${e['user_detail'][0]['username']} - ',
-                                      overflow: TextOverflow.ellipsis,
-                                    ),
+                            return Padding(
+                              padding: const EdgeInsets.fromLTRB(10, 0, 10, 5),
+                              child: Container(
+                                decoration: const BoxDecoration(
+                                  color: Color(0xfff1f3f4),
+                                  borderRadius: BorderRadius.all(Radius.circular(5))
+                                ),
+                                child: ListTile(
+                                  leading: CircleAvatar(
+                                    backgroundImage: NetworkImage(
+                                        'https://storage.googleapis.com/noseason/${e['user_detail'][0]['profile_image']}'),
                                   ),
-                                  Flexible(
-                                    child: Row(children: [
-                                      Icon(
-                                        Icons.pin_drop_rounded,
-                                        color: Colors.blue,
-                                        size: 14,
-                                      ),
+                                  title: e['topic'] != null
+                                      ? Text(e['topic'])
+                                      : Text(
+                                          '${e['mood_emoji']} ${e['mood_detail']}'),
+                                  subtitle: Row(
+                                    children: [
                                       Flexible(
                                         child: Text(
-                                            '${e['marker_detail'][0]['marker_id']}',
-                                            overflow: TextOverflow.ellipsis,
-                                            style: const TextStyle(
-                                                color: Colors.blue)),
-                                      )
-                                    ]),
-                                  ),
-                                ],
-                              ),
-                              trailing: Text(
-                                e['date'].toString().substring(0, 10),
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                              onTap: () {
-                                Navigator.push(
-                                    context,
-                                    PageTransition(
-                                        child: DiaryDetailPutdown(
-                                          id: e['_id'],
+                                          '@${e['user_detail'][0]['username']} - ',
+                                          overflow: TextOverflow.ellipsis,
                                         ),
-                                        type: PageTransitionType.rightToLeft));
-                              },
+                                      ),
+                                      Flexible(
+                                        child: Row(children: [
+                                          const Icon(
+                                            MdiIcons.mapMarker,
+                                            color: Colors.black54,
+                                            size: 14,
+                                          ),
+                                          Flexible(
+                                            child: Text(
+                                                '${e['marker_detail'][0]['marker_id']}',
+                                                overflow: TextOverflow.ellipsis,
+                                                style: const TextStyle(
+                                                    )),
+                                          )
+                                        ]),
+                                      ),
+                                    ],
+                                  ),
+                                  trailing: Text(
+                                    e['date'].toString().substring(0, 10),
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  onTap: () {
+                                    Navigator.push(
+                                        context,
+                                        PageTransition(
+                                            child: DiaryDetailPutdown(
+                                              id: e['_id'],
+                                            ),
+                                            type: PageTransitionType.rightToLeft));
+                                  },
+                                ),
+                              ),
                             );
                           }).toList(),
                         ),
